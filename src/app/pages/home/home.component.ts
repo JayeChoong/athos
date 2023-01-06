28bfa0cb62f51b35f5da9352bdfcf60aca1decb6
JayeChoong <102500577+JayeChoong@users.noreply.github.com>
2023-01-06 13:15:03 +0800

    new

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,22 +6,58 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit { 
+export class HomeComponent implements OnInit {
   slides = [
-    {image: './assets/carousel/D1.png'},
-    {image: './assets/carousel/D2.png'},
-    {image: './assets/carousel/D3.png'},
-    {image: './assets/carousel/D7.mp4', isVideo: true},
-    {image: './assets/carousel/D4.png'},
-    {image: './assets/carousel/D5.png'},
-    {image: './assets/carousel/D6.png'},
+    { image: './assets/carousel/R1.jpg', left: 0, top: 0 },
+    { image: './assets/carousel/R2.jpg', left: 25, top: 0 },
+    { image: './assets/carousel/R3.jpg', left: 50, top: 0 },
+    { image: './assets/carousel/R4.jpg', left: 60, top: 0 },
+    { image: './assets/carousel/R5.jpg', left: 80, top: 0 },
+    { image: './assets/carousel/R6.jpg', left: 0, top: 30 },
+    { image: './assets/carousel/R7.jpg', left: 15, top: 20 },
+    { image: './assets/carousel/R8.jpg', left: 35, top: 15 },
+    { image: './assets/carousel/R9.jpg', left: 25, top: 65 },
+    { image: './assets/carousel/R10.jpg', left: 75, top: 30 },
+    { image: './assets/carousel/R11.jpg', left: 20, top: 50 },
+    { image: './assets/carousel/R12.jpg', left: 20, top: 45 },
+    { image: './assets/carousel/R13.jpg', left: 35, top: 35 },
+    { image: './assets/carousel/R14.jpg', left: 60, top: 40 },
+    { image: './assets/carousel/R15.jpg', left: 80, top: 45 },
+    { image: './assets/carousel/R16.jpg', left: 0, top: 65 },
+    { image: './assets/carousel/R17.jpg', left: 15, top: 65 },
+    { image: './assets/carousel/R18.jpg', left: 40, top: 65 },
+    { image: './assets/carousel/R19.jpg', left: 55, top: 65 },
+    { image: './assets/carousel/R20.jpg', left: 80, top: 65 },
   ];
   slide: any;
 
-  constructor(private pS: ProductService) {
+  constructor(private pS: ProductService,
+  ) {
+
+    // let images = document.querySelectorAll(".img-text-container");
+
+    // images.forEach(image => {
+    //     let text = image.querySelectorAll(".black");
+
+    //     image.addEventListener("mouseover", () => {
+    //         text.forEach(item => {
+    //             item.classList.remove("black");
+    //             item.classList.add("white");
+    //         })
+    //     })
+
+    //     image.addEventListener("mouseleave", () => {
+    //         text.forEach(item => {
+    //             item.classList.add("black");
+    //             item.classList.remove("white");
+    //         })
+    //     })
+    // })
+
   }
 
   ngOnInit(): void {
+
     const video = document.querySelectorAll('video');
     const w = window.innerWidth;
     let myCarousel: any;
@@ -32,6 +68,9 @@ export class HomeComponent implements OnInit {
       video[0].muted = true;
       this.slide = 3;
     } else if (w <= 1250) {
+      for (const itm of this.slides) {
+        itm.left = itm.left-25;
+      }
       myCarousel = document.getElementById('mobileCarousel');
       video[2].play();
       video[2].muted = true;
@@ -47,6 +86,5 @@ export class HomeComponent implements OnInit {
         video[evt.to + 2].muted = true;
       };
     });
-
   }
 }
